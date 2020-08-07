b1ac27af8b49e75decc319366702cafaf9534122
Schrese <schreseholloway@gmail.com>
2020-08-07 13:32:34 -0400

    finished ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,21 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    connections_table = {}
 
+    route = [None] * length
+
+    for ticket in tickets:
+        connections_table[ticket.source] = ticket.destination
+
+    starting_point = connections_table['NONE']
+
+    ticket_pointer = starting_point
+
+    for i in range(0, len(connections_table)):
+        # print(i) 
+        route[i] = ticket_pointer
+        ticket_pointer = connections_table[ticket_pointer]
+        
+    print(route)
     return route
