606c14554b0a81e34146c7433470c92d49a37e04
Schrese <schreseholloway@gmail.com>
2020-08-07 13:08:18 -0400

    Finished 1 and 3

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,34 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    table = {}
+    if length == 1:
+        return
 
+    for i, weight in enumerate(weights):
+        print(f"{weight} at index {i}")
+        table[weight] = i
+
+    for weight in weights:
+        answer = [None] * 2
+        entry_to_find = limit - weight
+        if entry_to_find in table:
+            print(f"entry to find: {entry_to_find}, weight: {weight}")
+            if (length == 2) and (weight == entry_to_find):
+                print(f'edge-case: {table[weight]}')
+                answer[0] = table[weight]
+                answer[1] = weights.index(weight)
+            elif weight == limit:
+                answer[0] = table[entry_to_find]
+                answer[1] = table[weight]
+            else:
+                if weight >= entry_to_find:
+                    answer[0] = table[weight]
+                    answer[1] = table[entry_to_find]
+                else:
+                    answer[0] = table[entry_to_find]
+                    answer[1] = table[weight]
+            print(answer, 'the answer')
+            return answer
     return None
+
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,8 +3,26 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    present_in_all_arrays = []
+    num_of_arrays = len(arrays)
 
-    return result
+    table = {}
+    def build_table(arrays):
+        for array in arrays:
+            for number in array:
+                if number not in table:
+                    table[number] = 1
+                else:
+                    table[number] += 1 # for some reason I couldn't use the default else (as in... just not indent it after the if. Interesting)
+
+    build_table(arrays)
+    
+    for entry in table:
+        # print(f"entry is: {entry}, entry value is: {table[entry]}")
+        if table[entry] >= num_of_arrays:
+            present_in_all_arrays.append(entry)
+    print(present_in_all_arrays)
+    return present_in_all_arrays
 
 
 if __name__ == "__main__":
